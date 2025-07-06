e04f4b354cf6516a1e94c37e242b30b24361d78c
liangk <liangk.tw@gmail.com>
2025-07-06 05:31:50 +0000

    Add basic layout

--- schematic-mappnig-app/src/app/app.ts
+++ schematic-mappnig-app/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Nav } from './layout/nav/nav';
+import { MainContent } from './layout/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Nav, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
